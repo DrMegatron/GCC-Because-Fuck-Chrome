69a0e3e94bf0a2af110efcf02116538aeb29e44d
DrMegatron <aryazokaie@gmail.com>
2021-03-25 02:08:19 -0700

    idsafjugkljakl;sjfdklAJDFALK;JDF

---
+++ gcc-8.2/lib/gcc/x86_64-linux-gnu/8.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-8.2.0/configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=/usr/local/gcc-8.2 --enable-checking=release --enable-languages=c,c++,fortran --disable-multilib --program-suffix=-8.2";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
